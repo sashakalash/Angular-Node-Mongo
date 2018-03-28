0604c13caa3896437cc97e35378de14662ee7c2a
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-03-28 21:23:57 +0300

    +

--- stream/stream.js
+++ stream/stream.js
@@ -1,22 +1,30 @@
 const crypto = require('crypto');
 const hash = crypto.createHash('md5');
+const fs = require('fs');
 
-const readable = require('stream').Readable;
+const input = fs.createReadStream('data.txt');
+const output = fs.createWriteStream('out_data.txt');
 
-class cReadable extends readable {
-  constructor(options) {
-    super(options);
-    this.count = 0;
+const hashTransform = crypto.createHash('md5');
+const Transform = require('stream').Transform;
+
+class CTransform extends Transform {
+  constructor (data) {
+    super();
+    const enc = {encoding: 'hex'};
   }
-  _read() {
-    const result = this.push(this.count.toString());
+  _transform(data, , ) {
+    return this.hash.digest('hex');
   }
 }
 
-const input = new cReadable();
-input.on('readable', () => {
-  let data;
-  // while (data = input.read()) {
-  //   console.log(data);
-  // }
-});
+const tf = new CTransform();
+
+input.pipe(hash);
+hash.pipe(tf._transform);
+hash.pipe(output);
+/*от аспиранта узнал, что лучше сохранить в переменную результат pipe, а потом с ним работать. 
+у меня работает и так, прошу объяснить, если является ошибкой*/
+hash.pipe(process.stdout);
+
+
