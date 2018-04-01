46c601047eea6837ed21bbfecaf01be4023c83cb
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-04-01 13:53:07 +0300

    corrected transform class safe memory logic

--- stream/streamTransform.js
+++ stream/streamTransform.js
@@ -1,5 +1,4 @@
 const crypto = require('crypto');
-const hash = crypto.createHash('md5');
 const fs = require('fs');
 
 const input = fs.createReadStream('data.txt');
@@ -8,13 +7,18 @@ const output = fs.createWriteStream('out_data_transform.txt');
 const Transform = require('stream').Transform;
 
 class CTransform extends Transform {
-  constructor (options) {
-    super(options);
+  constructor (type = 'md5') {
+    super();
+    this.hash = crypto.createHash(type);
+    this.hashOutData = '';
   }
   _transform(chunk, encoding, callback) {
-    const hashUpd = hash.update(chunk);
-    this.push(hashUpd.digest('hex'));
+    this.hashOutData = this.hash.update(chunk);
     callback();
+  }
+  _flush(callback) {
+    callback(null, this.push(this.hashOutData.digest('hex')));
+    delete this.hash;
   }
 }
 
