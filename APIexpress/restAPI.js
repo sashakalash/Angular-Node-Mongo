6b35130c5fa6c599f4975d7703650a3304c6c0b6
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-04-08 16:15:23 +0300

    added restAPI

--- APIexpress/restAPI.js
+++ APIexpress/restAPI.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const fetch = require('node-fetch');
 const users = require('./users').users;
 
 app.use(bodyParser.json());
