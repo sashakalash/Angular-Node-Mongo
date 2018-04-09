1a05fc7fdb945363d1fe4d132c5a5f32e2ad3815
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-04-09 22:40:00 +0300

    added routeExpress.js

---
+++ routeExpress/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const ctlrEmptyReq = (req, res) => {
+  res.status(200);
+  res.send('Hello, Express.js');
+};
+
+const ctlrHelloReq = (req, res) => {
+  res.status(200);
+  res.send('Hello stranger!');
+};
+
+const ctlrHelloNameReq = (req, res) => {
+  res.status(200);
+  res.send(`Hello ${req.params.name}!`);
+};
+
+const ctlrAnyReq = (req, res) => {
+  res.status(200);
+  res.send(`You requested URI: ${req.url}`);
+};
+
+const middleCheckHed = (req, res, next) => {
+  if (!req.header('Key')) {
+    req.auth = false;
+  }
+  next();
+};
+
+const ctrlPost = (req, res) => {
+  if (!req.auth) {
+    res.sendStatus(401);
+    return;
+  }
+  if (Object.keys(req.body).length > 0) {
+    res.json(req.body);
+  } else {
+    res.sendStatus(404);
+  }    
+
+};
+
+app.get('/', ctlrEmptyReq);
+app.get('/hello', ctlrHelloReq);
+app.get('/hello/:name', ctlrHelloNameReq);
+app.all('/sub/*', ctlrAnyReq);
+app.post('/post', middleCheckHed, ctrlPost);
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
