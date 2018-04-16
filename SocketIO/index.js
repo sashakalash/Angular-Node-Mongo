e48a3f4af9d4b0b429d5748d8817e4530db697c4
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-04-16 22:33:54 +0300

    added socket API

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -1,9 +1,11 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.use(express.static('public'));
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
@@ -12,18 +14,27 @@ http.listen(3000, () => {
 const io = require('socket.io')(http);
 
 let numUsers = 0;
-let addedUser = false;
-
-
-
 
 io.on('connection', socket => {
+  let addedUser = false;
+   
+  socket.on('create', (roomNumber) => {
+    socket.broadcast.to(socket.room).emit('chat message', `${socket.nickname} has left ${socket.room}`);
+    socket.leave(socket.room);
+    socket.join(roomNumber);
+    socket.to(roomNumber).emit('chat message', `You've  joined to ${roomNumber}`);
+    socket.room = roomNumber;
+  });
+  
   socket.on('added user', (nickname) => {
     if (addedUser) return;
     socket.nickname = nickname;
     numUsers++;
     addedUser = true;
+    socket.join('main');
+    socket.room = 'main';
     socket.emit('login', {
+      room: socket.room,
       numUsers: numUsers
     });
     socket.broadcast.emit('user joined', {
@@ -32,8 +43,7 @@ io.on('connection', socket => {
     });
   });
 
-
   socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+    io.to(socket.room).emit('chat message', msg);
   });
 });
