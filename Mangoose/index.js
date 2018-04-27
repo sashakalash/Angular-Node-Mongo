43f645e21ec599ed868ccac61f9a5b22918869e5
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2018-04-27 21:04:11 +0300

    added index.js

--- Mangoose/index.js
+++ Mangoose/index.js
@@ -17,17 +17,39 @@ const tasksScheme = new Schema({
   name: String, 
   descrition: String, 
   status: Boolean, 
-  user: Schema.Types.ObjectId,
+  user: Schema.Types.ObjectId
 });
 
-tasksScheme.method.changeStatus = (id, cb) => {
-  return this.findById(id, cb);
-    
+tasksScheme.methods.changeStatus = (id) => {
+  return new Promise((done, fail) => {
+    Task.findById(id, (err, res) => {
+      if (err) {
+        fail(err);
+      }
+      res.status = res.status? 'false': 'true';
+      res.save((err, result) => err? fail(err): done());
+    });
+  });  
 };
+
+tasksScheme.methods.delegate = (taskId, userId) => {
+  return new Promise((done, fail) => {
+    Task.findById(taskId, (err, res) => {
+      if (err) {
+        fail(err);
+      }
+      res.user = new ObjectId(userId);
+      res.save((err, result) => err? fail(err): done());
+    });
+  });  
+};
+
 
 const User = mongoose.model("User", userScheme);
 const Task = mongoose.model("Tasks", tasksScheme);
 const url = 'mongodb://localhost:27017/TaskList';
+
+
 
 const callbackDB = (err, response, answer) => {
   if (err) {
@@ -109,16 +131,24 @@ app.get('/show/:reqType', (req, res) => {
   obj.find((err, result) => callbackDB(err, res, result));
 });
 
-
-
 app.post('/change_status', (req, res) => {
-  Task.changeStatus(req.body.id, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(result)
-  });
-    
+  mongoose.connect(url);
+  new Task().changeStatus(req.body.id)
+    .then(result => callbackDB(null, res, `Статус задачи ${req.body.id} изменен`))
+    .catch(err => callbackDB(err, res));
+});
+
+app.post('/delegate', (req, res) => {
+  mongoose.connect(url);
+  new Task().delegate(req.body.taskId, req.body.userId)
+    .then(result => callbackDB(null, res, `Задача ${req.body.taskId} делегирована на ${req.body.userId}`))
+    .catch(err => callbackDB(err, res));
+});
+
+app.post('/find', (req, res) => {
+  mongoose.connect(url);
+  const query = req.body.name? {name:req.body.name}: {description:req.body.description};
+  Task.find(query, (err, result) => callbackDB(err, res, result));
 });
 
 app.listen(PORT, () => console.log(`App started on ${PORT} port`));
